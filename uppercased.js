fe92beb1a72b92baa7f55978177d7ddf8b724b15
parmeno <angelparmeno@gmail.com>
2016-10-11 08:55:48 +0200

    ok

--- uppercased.js
+++ uppercased.js
@@ -1,28 +1,8 @@
 
-/*
-
-var http = require('http');
-var map  = require('through2-map');
-
-var server = http.createServer(function(req, res) {
-    if(req.method !== 'POST')
-        return res.end('POST please');
-
-    req.pipe(map(function(chunk) {
-        return chunk.toString().toUpperCase();
-    })).pipe(res);
-});
-
-server.listen(8000); */
-
-
-
-
-
 const http = require('http');
 
 var server = http.createServer(function(req, res) {
-    //if(req.method != "POST") resp.end('I respond to POST only');
+    if(req.method != "POST") resp.end('I respond to POST only');
 
     var body = '';
     req.setEncoding('utf8');
